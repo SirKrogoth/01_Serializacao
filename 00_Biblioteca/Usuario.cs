6b7579278f0a8039b962a0c1c98f22b7adb569da
joao.fagundes <joao.fagundes@DSKDEV08>
2018-07-24 14:51:08 -0300

    Criada classe Usuario.cs

---
+++ 00_Biblioteca/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00_Biblioteca
+{
+    public class Usuario
+    {
+        private string nome { get; set; }
+        private string cpf { get; set; }
+        private string email { get; set; }
+    }
+}
