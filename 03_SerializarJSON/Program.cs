ada1889d63d2fcd59a3dc301f4cf47162e7848da
joao.fagundes <joao.fagundes@DSKDEV08>
2018-07-25 12:47:35 -0300

    Serializar JSON

---
+++ 03_SerializarJSON/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;//utilizado para JSON
+using _00_Biblioteca;
+using System.IO;
+
+namespace _03_SerializarJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario() { nome = "Caroline Kisner da Silva", cpf = "007.215.411-22", email = "CarolineKisner@teleconsistemas.com.br" };
+
+            JavaScriptSerializer serializer = new JavaScriptSerializer();            
+
+            string objetoSerializado = serializer.Serialize(usuario);
+
+            StreamWriter writer = new StreamWriter(@"C:\Telecon_Sistemas\tomate.json");
+            
+            writer.WriteLine(objetoSerializado);
+
+            writer.Close();
+        }
+    }
+}
